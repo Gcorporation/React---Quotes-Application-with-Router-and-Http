aabed565d17e7214ec68ddece17ce459c7b1d7b3
Prabhjot Singh <prabhjotsingh394@gmail.com>
2021-10-24 12:58:44 +0530

    redirecting and adding Params

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+import { Fragment } from "react";
+
+const QuoteDetail = () => {
+
+    const params  = useParams();
+
+    return (
+        <Fragment>
+            <h1>Quote Details!</h1>
+            <p>{params.quoteId}</p>
+        </Fragment>
+    );
+};
+
+export default QuoteDetail;
