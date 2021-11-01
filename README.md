0d4471818a66ae960ad81ff80f34883861c26188
Prabhjot Singh <prabhjotsingh394@gmail.com>
2021-11-01 12:04:56 +0530

    Added firebase and deployment link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React---Quotes-Application-with-Router-and-Http
 
 simply download or clone the package. Run npm install followed by npm start :)
+
+The app is hosted here : https://react-quotes-e47c9.web.app/quotes
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,30 +1,60 @@
-import { useParams, Route, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import { Fragment, useEffect } from "react";
 import Comments from '../components/comments/Comments';
-
+import useHttp from '../hooks/hooks/use-http';
 import HighLightesQuote from '../components/quotes/HighlightedQuote';
+import { getSingleQuote } from'../lib/lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is Fun!' },
+    { id: 'q2', author: 'Tax', text: 'Learning React is Blahh!' },
+    { id: 'q3', author: 'Snax', text: 'Learning React is SHhhhhhhhh!' },
+];
 
 const QuoteDetail = () => {
 
-    const DUMMY_QUOTES = [
-        { id: 'q1', author: 'Max', text: 'Learning React is Fun!' },
-        { id: 'q2', author: 'Tax', text: 'Learning React is Blahh!' },
-        { id: 'q3', author: 'Snax', text: 'Learning React is SHhhhhhhhh!' },
-    ];
-
     const params  = useParams();
+    const match = useRouteMatch();
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    const { quoteId } = params;
+
+    const { sendRequest, status, data: loadedQuotes, error } = useHttp(getSingleQuote, true);
+
+    useEffect(()=>{
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    console.log(match);
+
+    if(status === 'pending') {
+        return (
+            <div className="centered">
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if(error) {
+        return (
+            <p className='centered focused'>{error}</p>
+        )
+    }
+
+    if(!loadedQuotes.text) {
+        return <p>No Quote Found!</p>
+    }
 
     return (
         <Fragment>
-            <HighLightesQuote text={quote.text} author={quote.author} />
+            <HighLightesQuote text={loadedQuotes.text} author={loadedQuotes.author} />
             <Route path={`/quotes/${params.quoteId}`} exact>
                 <div className='centered'>
                     <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
                 </div>
             </Route>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </Fragment>
